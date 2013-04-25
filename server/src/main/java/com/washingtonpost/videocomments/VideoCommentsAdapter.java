36de0f4ccd32a13bc0201a9dc0bd5ad1b0569ab9
Sergey Shramko <s.shramko@eastbanctech.ru>
2013-04-26 01:26:23 +0700

    Add red5 sources.

--- server/src/main/java/com/washingtonpost/videocomments/VideoCommentsAdapter.java
+++ server/src/main/java/com/washingtonpost/videocomments/VideoCommentsAdapter.java
@@ -3,29 +3,29 @@ package com.washingtonpost.videocomments;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.red5.server.adapter.ApplicationAdapter;
-import org.red5.server.api.IConnection;
+//import org.red5.server.adapter.ApplicationAdapter;
+//import org.red5.server.api.IConnection;
 
-public class VideoCommentsAdapter extends ApplicationAdapter {
+public class VideoCommentsAdapter {
 
     private static final Log log = LogFactory.getLog(VideoCommentsAdapter.class);
 
-    public boolean appStart() {
-        log.info("VideoComments.appStart");
-        return true;
-    }
-
-    public void appStop() {
-        log.info("VideoComments.appStop");
-    }
-
-    public boolean appConnect(IConnection conn, Object[] params) {
-        log.info("VideoComments.appConnect " + conn.getClient().getId());
-        return true;
-    }
-
-    public void appDisconnect(IConnection conn, Object[] params) {
-        log.info("VideoComments.appDisconnect " + conn.getClient().getId());
-    }
+//    public boolean appStart() {
+//        log.info("VideoComments.appStart");
+//        return true;
+//    }
+//
+//    public void appStop() {
+//        log.info("VideoComments.appStop");
+//    }
+//
+//    public boolean appConnect(IConnection conn, Object[] params) {
+//        log.info("VideoComments.appConnect " + conn.getClient().getId());
+//        return true;
+//    }
+//
+//    public void appDisconnect(IConnection conn, Object[] params) {
+//        log.info("VideoComments.appDisconnect " + conn.getClient().getId());
+//    }
 
 }
