ef102823be43e3be5d398cd9d1f2e72af90843df
Sergey Shramko <s.shramko@eastbanctech.ru>
2013-04-29 16:05:50 +0700

    #4543 Create model and DAO for storing embed codes.

---
+++ server/src/main/java/com/washingtonpost/videocomments/model/VideoComment.java
@@ -0,0 +1,67 @@
+package com.washingtonpost.videocomments.model;
+
+import java.util.Date;
+
+public class VideoComment {
+
+    private Long id;
+    private boolean complete;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VideoComment that = (VideoComment) o;
+
+        if (complete != that.complete) return false;
+        if (createdAt != null ? !createdAt.equals(that.createdAt) : that.createdAt != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (updatedAt != null ? !updatedAt.equals(that.updatedAt) : that.updatedAt != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (complete ? 1 : 0);
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        result = 31 * result + (updatedAt != null ? updatedAt.hashCode() : 0);
+        return result;
+    }
+}
